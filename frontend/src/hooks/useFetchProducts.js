718e15ebe460107515fe4a259c2b6e7f44964738
Mirko <mirko.weibel@navigil.com>
2024-11-12 17:06:34 +0200

    env

--- frontend/src/hooks/useFetchProducts.js
+++ frontend/src/hooks/useFetchProducts.js
@@ -8,7 +8,7 @@ const useFetchProducts = () => {
        const fetchProducts = async () => {
          setLoading(true);
          try {
-           const response = await fetch(`http://localhost:5000/api/products/`);
+           const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/products/`);
            const data = await response.json();
            const products = data.data || []; // Adjusting in case 'data' is already an array
            
